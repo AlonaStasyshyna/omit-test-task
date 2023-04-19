d07f255de9e9a4b15ab136974852b01bfc25d5cb
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2023-04-19 14:04:50 +0300

    Add medias and colors for stylization

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,11 +1,55 @@
 export const lightTheme = {
+  media: {
+    mobile: '(min-width: 320px)',
+    mobileOnly: '(max-width: 767px)',
+    tablet: '(min-width: 768px)',
+    tabletOnly: '(min-width: 768px) and (max-width: 1439px)',
+    desktop: '(min-width: 1440px)',
+    notDesktop: '(max-width: 1439px)',
+  },
+
   colors: {
-    background: '#EBEBEB',
+    background: '#FFFFFF',
+    mainLogoBackground: '#FFFFFF',
+    white: '#FFFFFF',
+    primaryText: '#424242',
+    secondTitle: '#999B9A',
+    filterInputBackground: 'rgba(52, 52, 52, 0.1)',
+    filterText: '#343434',
+    gold: '#DCCE90',
+    green: '#29B4AD',
+    grayStroke: '2px solid #B5B7B6',
+    pagination: '#343434',
+    accentSection: '#424242',
+    darkGreen: '#3D948C',
+    footerText: '#929292',
   },
 };
 
 export const darkTheme = {
+  media: {
+    mobile: '(min-width: 320px)',
+    mobileOnly: '(max-width: 767px)',
+    tablet: '(min-width: 768px)',
+    tabletOnly: '(min-width: 768px) and (max-width: 1439px)',
+    desktop: '(min-width: 1440px)',
+    notDesktop: '(max-width: 1439px)',
+  },
+
   colors: {
-    background: '#000000',
+    background: '#343434',
+    mainLogoBackground: '#414141',
+    white: '#FFFFFF',
+    primaryText: '#FFFFFF',
+    secondTitle: '#999B9A',
+    filterInputBackground: 'rgba(255, 255, 255, 0.1)',
+    filterText: '#FFFFFF',
+    gold: '#DCCE90',
+    green: '#29B4AD',
+    grayStroke: '2px solid #B5B7B6',
+    pagination: 'rgba(255, 255, 255, 0.1)',
+    accentSection: '#424242',
+    darkGreen: '#3D948C',
+    footerText: '#929292',
   },
 };
